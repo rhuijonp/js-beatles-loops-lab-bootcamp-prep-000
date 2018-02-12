7ebb48380c48607cdfc81593b5c4ae3bf89009d6
Rafael Huijon <rhuijonp@gmail.com>
2018-02-12 17:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function theBeatlesPlay(musicians, instruments){
   var newArray = [];
   
   for (var i = 0; i < musicians.length; i++){
-    newArray.push(musicians[i] + " plays " + instruments[0])
+    newArray.push(musicians[i] + " plays " + instruments[0]);
   }
   
+  return newArray;
   
 }
 
